b0ed0205964d1eb5056d8975258470cae6862ea3
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-10-14 12:34:22 +0900

    feat: impl home timeline page

--- app/components/note.tsx
+++ app/components/note.tsx
@@ -1,10 +1,36 @@
-import styles from "~/components/note.module.css"
+import styles from "~/components/note.module.css";
 
-export const Note = () => {
-  return (<div className={styles.note}>
-    <div className={styles.accountNameContainer}>
-      <h2>Nickname <span>@username@example.test</span></h2>
-    </div>
-    <p>hello hello hello hello hello hello hello hello hello </p>
-  </div>)
+export interface NoteProps {
+  content: string;
+  contentsWarningComment: string;
+  author: {
+    avatar: string;
+    name: string;
+    nickname: string;
+  };
 }
+
+export const Note = ({
+  content,
+  contentsWarningComment,
+  author,
+}: NoteProps) => {
+  return (
+    <div className={styles.note}>
+      <div className={styles.accountNameContainer}>
+        <h2>
+          {author.nickname}
+          <span>{author.name}</span>
+        </h2>
+      </div>
+      {[...contentsWarningComment].length !== 0 ? (
+        <details>
+          <summary>{contentsWarningComment}</summary>
+          <p>{content}</p>
+        </details>
+      ) : (
+        <p>{content}</p>
+      )}
+    </div>
+  );
+};
--- app/routes/timeline.tsx
+++ app/routes/timeline.tsx
@@ -1,29 +1,71 @@
 import { MetaFunction } from "@remix-run/react";
+import { useEffect, useState } from "react";
 import { Note } from "~/components/note";
 import styles from "~/styles/timeline.module.css";
 
-export const meta: MetaFunction = () => {
-  return [
-    {title: "Timeline | Caramel"},
-    {content: "noindex"}
-  ]
+interface HomeTimelineResponse {
+  id: string;
+  content: string;
+  contents_warning_comment: string;
+  visibility: "PUBLIC" | "HOME" | "FOLLOWERS";
+  created_at: Date;
+  author: {
+    id: string;
+    name: string;
+    display_name: string;
+    bio: string;
+    avatar: string;
+    header: string;
+    followed_count: number;
+    following_count: number;
+  };
 }
 
+export const meta: MetaFunction = () => {
+  return [{ title: "Timeline | Caramel" }, { content: "noindex" }];
+};
+
 export default function Timeline() {
+  const [notes, setNotes] = useState<HomeTimelineResponse[]>();
+  const [error, setError] = useState<string>("loading...");
+  useEffect(() => {
+    const fetchTimeline = async () => {
+      const timelineRes = await fetch("http://localhost:3000/timeline/home", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      });
+      if (!timelineRes.ok) {
+        const errorJSON = await timelineRes.json();
+        return setError(errorJSON.error);
+      }
+      const timelineData = (await timelineRes.json()) as HomeTimelineResponse[];
+      setNotes(timelineData);
+    };
+    fetchTimeline();
+  }, []);
+
   return (
     <div className={styles.noteContainer}>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
+      {notes ? (
+        notes.map((note) => {
+          const author = {
+            avatar: note.author.avatar,
+            name: note.author.name,
+            nickname: note.author.display_name,
+          };
+          return (
+            <Note
+              key={note.id}
+              author={author}
+              content={note.content}
+              contentsWarningComment={note.contents_warning_comment}
+            />
+          );
+        })
+      ) : (
+        <div>{error}</div>
+      )}
     </div>
-  )
+  );
 }
