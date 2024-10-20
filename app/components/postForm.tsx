1afd4073276181b29f007228cbf0e922276d7828
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-10-20 23:31:51 +0900

    feat: 投稿ができるように (#29)
    * feat: impl note post form
    
    * chore: rename api.notes.create.ts -> api.notes.ts

---
+++ app/components/postForm.tsx
@@ -0,0 +1,40 @@
+import { useFetcher } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { action } from "~/routes/api.notes";
+
+export const PostForm = () => {
+  const [content, setContent] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const fetcher = useFetcher<typeof action>();
+
+  useEffect(() => {
+    if (!fetcher.data) return;
+
+    if (fetcher.state === "loading") {
+      if ("error" in fetcher.data) {
+        setErrorMessage(fetcher.data.error);
+        return;
+      }
+      setContent("");
+    }
+  }, [fetcher.state]);
+
+  return (
+    <fetcher.Form method="post" action="/api/notes">
+      <textarea
+        required
+        name="content"
+        id="content"
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+      ></textarea>
+      <select id="visibility" name="visibility">
+        <option value="PUBLIC">Public</option>
+        <option value="HOME">Home</option>
+        <option value="FOLLOWERS">Followers</option>
+      </select>
+      <button type="submit">Submit</button>
+      <p>{errorMessage}</p>
+    </fetcher.Form>
+  );
+};
