c741b133fd1b2ef74a3d31886dee2f3f2c723373
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-11-03 14:45:13 +0900

    feat: impl account page

---
+++ app/lib/account.ts
@@ -0,0 +1,47 @@
+import { TimelineResponse } from "~/lib/timeline";
+
+export interface AccountResponse {
+  id: string;
+  name: string;
+  nickname: string;
+  bio: string;
+  avatar: string;
+  header: string;
+  followed_count: number;
+  following_count: number;
+  note_count: number;
+}
+
+export const account = async (
+  identifier: string,
+  token: string | undefined
+): Promise<AccountResponse | { error: string }> => {
+  const res = await fetch(`http://localhost:3000/accounts/${identifier}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
+    return { error: "failed to fetch" };
+  }
+  return (await res.json()) as AccountResponse;
+};
+
+export const accountTimeline = async (
+  id: string,
+  token: string | undefined
+): Promise<TimelineResponse[] | { error: string }> => {
+  try {
+    const res = await fetch(`http://localhost:3000/timeline/accounts/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!res.ok) {
+      return (await res.json()) as { error: string };
+    }
+    return (await res.json()) as TimelineResponse[];
+  } catch {
+    return { error: "unknown error" };
+  }
+};
---
+++ app/routes/accounts.$id.tsx
@@ -0,0 +1,111 @@
+import { LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { MetaFunction, useLoaderData } from "@remix-run/react";
+import { Note, NoteProps } from "~/components/note";
+import { account, AccountResponse, accountTimeline } from "~/lib/account";
+import { accountCookie } from "~/lib/login";
+import { TimelineResponse } from "~/lib/timeline";
+import styles from "~/styles/account.module.css";
+
+export const loader = async ({
+  request,
+  params,
+}: LoaderFunctionArgs): Promise<
+  { error: string } | { account: AccountResponse; timeline: TimelineResponse[] }
+> => {
+  const token = await accountCookie.parse(request.headers.get("Cookie"));
+  if (!token) {
+    return { error: "not logged in" };
+  }
+  if (!params.id) return { error: "invalid id" };
+
+  const accountRes = await account(params.id, token);
+  if ("error" in accountRes) {
+    return { error: accountRes.error };
+  }
+
+  const timelineRes = await accountTimeline(accountRes.id, token);
+  if ("error" in timelineRes) {
+    return { error: timelineRes.error };
+  }
+
+  return { account: accountRes, timeline: timelineRes };
+};
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  if (!data) {
+    return [{ title: "Account | Caramel" }, { content: "noindex" }];
+  }
+  if ("error" in data) {
+    return [{ title: "Account | Caramel" }, { content: "noindex" }];
+  }
+
+  return [
+    { title: `${data.account.nickname} (${data.account.name}) | Caramel` },
+    { content: "noindex" },
+  ];
+};
+
+export default function Account() {
+  const data = useLoaderData<typeof loader>();
+  if ("error" in data) {
+    return <div>{data.error}</div>;
+  }
+
+  const timelineNotes = data.timeline.map(
+    (note): NoteProps => ({
+      id: note.id,
+      content: note.content,
+      contentsWarningComment: note.contents_warning_comment,
+      author: {
+        avatar: note.author.avatar,
+        name: note.author.name,
+        nickname: note.author.display_name,
+      },
+      reactions: note.reactions.map((reaction) => ({
+        emoji: reaction.emoji,
+        reactedBy: reaction.reacted_by,
+      })),
+      loggedInAccountID: data.account.id,
+    })
+  );
+
+  return (
+    <>
+      <div className={styles.accountData}>
+        <div className={styles.avatarImage}>
+          <img
+            src={data.account.avatar}
+            alt={`${data.account.nickname}'s avatar image`}
+            loading={"lazy"}
+          />
+        </div>
+        <h1>
+          {data.account.nickname} <span>{data.account.name}</span>
+        </h1>
+        <p>{data.account.bio}</p>
+        <div>
+          <p>
+            <span>{data.account.following_count}</span> following
+            <span>{data.account.followed_count}</span> followers
+          </p>
+        </div>
+      </div>
+
+      <AccountTimeline notes={timelineNotes} />
+    </>
+  );
+}
+
+interface AccountTimelineProps {
+  notes: NoteProps[];
+}
+
+export const AccountTimeline = ({ notes }: AccountTimelineProps) => {
+  return (
+    <div className={styles.accountTimelineContainer}>
+      {notes.map((note) => {
+        return <Note key={note.id} {...note} />;
+      })}
+    </div>
+  );
+};
