e6b954ea568e70feb3d7a0ae8a0081ea2038d7e9
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-11-23 19:00:29 +0900

    feat: impl sign out (#71)
    * feat: impl sign out
    
    * chore: remove unnecesary log output

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,44 @@
-import { Link } from "@remix-run/react";
+import { LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { Link, useLoaderData } from "@remix-run/react";
+import { accountCookie } from "~/lib/login";
+import { parseToken } from "~/lib/parseToken";
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs): Promise<{ isLoggedIn: boolean }> => {
+  const token = await accountCookie.parse(request.headers.get("Cookie"));
+  if (!token) {
+    return { isLoggedIn: false };
+  }
+
+  const parsedToken = parseToken(token);
+  if (parsedToken instanceof Error) {
+    return { isLoggedIn: false };
+  }
+
+  return { isLoggedIn: true };
+};
 
 export default function Index() {
+  const isLoggedIn = useLoaderData<typeof loader>();
+
   return (
     <div>
       <h1>Caramel: Pulsate minimal web interface</h1>
       <p>Hello world</p>
       <nav>
-        <Link to="/timeline">Timeline</Link> <Link to="/login">Login</Link>
+        <ul>
+          <li>
+            <Link to="/timeline">Timeline</Link>
+          </li>
+          <li>
+            {isLoggedIn.isLoggedIn ? (
+              <Link to="/logout">Sign out</Link>
+            ) : (
+              <Link to="/login">Sign in</Link>
+            )}
+          </li>
+        </ul>
       </nav>
     </div>
   );
