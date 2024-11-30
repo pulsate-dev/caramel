8c52f01ff48b7f67cd3e8ca34d9863e5f2cf6e1f
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-11-30 15:53:12 +0900

    feat: impl navigation sidebar (#88)

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import { SideBar } from "~/components/sideBar";
 import styles from "~/root.module.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -10,9 +11,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className={styles.root}>
-        {children}
-        <Scripts />
+      <body>
+        <div className={styles.center}>
+          <div className={styles.layout}>
+            <SideBar />
+            <div className={styles.root}>
+              {children}
+              <Scripts />
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
