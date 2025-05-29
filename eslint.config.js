a07af72b6f208c00de19e2d31af0c4407dddebed
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-05-29 23:29:05 +0900

    chore: ignore auto-generated files from lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
-  {files: ["app/**/*.{ts,tsx}"]},
-  {ignores: ["build/**", "node_modules/**"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["app/**/*.{ts,tsx}"] },
+  { ignores: ["build/**", "node_modules/**", ".react-router/**"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
@@ -15,13 +15,13 @@ export default [
     settings: {
       react: {
         version: "detect",
-      }
-    }
+      },
+    },
   },
   {
     rules: {
       "react/react-in-jsx-scope": "off",
-    }
+    },
   },
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ];
