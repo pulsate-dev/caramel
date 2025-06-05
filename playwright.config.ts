2a7798a46d35192e94df62e75f2c82cf8054326d
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-06-05 09:17:30 +0900

    Setup playwright

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const isCI = !!process.env.CI;
+
+export default defineConfig({
+  testDir: "tests",
+  fullyParallel: true,
+  forbidOnly: isCI,
+  retries: isCI ? 2 : 0,
+  workers: isCI ? 1 : undefined,
+  use: {
+    // Base URL to use in actions like `await page.goto('/')`.
+    baseURL: "http://localhost:5173",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
