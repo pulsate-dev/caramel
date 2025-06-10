be698057ae59df315f3b1c68d861f1af9d19d2c9
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-06-11 00:02:17 +0900

    chore: フォロー/フォロワーを取得するAPIクライアントを実装

---
+++ app/lib/api/followlist.ts
@@ -0,0 +1,85 @@
+import type { GetV0AccountsIdFollowerResponse, GetV0AccountsIdFollowingResponse } from "@pulsate-dev/exp-api-types";
+
+interface FollowResponseBase {
+  id: string;
+  name: string;
+  nickname: string;
+  avatarURL: string;
+}
+
+export type FollowingResponse = FollowResponseBase;
+export type FollowerResponse = FollowResponseBase;
+
+export async function getFollowingList(
+  basePath: string,
+  token: string,
+  accountID: string
+): Promise<
+  { isSuccess: true; response: FollowingResponse[] } | { isSuccess: false }
+> {
+  try {
+    const followingRes = await fetch(
+      new URL(`/v0/accounts/${accountID}/following`, basePath),
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (!followingRes.ok) {
+      return { isSuccess: false };
+    }
+
+    const accounts =
+      (await followingRes.json()) as GetV0AccountsIdFollowingResponse;
+
+    return {
+      isSuccess: true,
+      response: accounts.map((account) => ({
+        id: account.id,
+        name: account.name,
+        nickname: account.nickname,
+        avatarURL: account.avatar,
+      })),
+    };
+  } catch {
+    return { isSuccess: false };
+  }
+}
+
+export async function getFollowersList(
+  basePath: string,
+  token: string,
+  accountID: string
+): Promise<
+  { isSuccess: true; response: FollowerResponse[] } | { isSuccess: false }
+> {
+try {
+    const followerRes = await fetch(
+      new URL(`/v0/accounts/${accountID}/follower`, basePath),
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (!followerRes.ok) {
+      return { isSuccess: false };
+    }
+
+    const accounts =
+      (await followerRes.json()) as GetV0AccountsIdFollowerResponse;
+
+    return {
+      isSuccess: true,
+      response: accounts.map((account) => ({
+        id: account.id,
+        name: account.name,
+        nickname: account.nickname,
+        avatarURL: account.avatar,
+      })),
+    };
+  } catch {
+    return { isSuccess: false };
+  }
+}
