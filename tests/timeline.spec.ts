71f3e074a2982a0c15c11d1a384b4e32009f1e96
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-01-20 22:31:50 +0900

    Update test case

--- tests/timeline.spec.ts
+++ tests/timeline.spec.ts
@@ -2,14 +2,14 @@ import { expect, test } from "@playwright/test";
 
 test("has account info", async ({ page }) => {
   await page.goto("/login");
-  await page.locator("#name").fill("@john@example.com");
+  await page.locator("#email").fill("testuser101@example.com");
   await page.locator("#password").fill("じゃすた・いぐざんぽぅ");
   await page.locator("#password + [type='submit']").click();
   await page.waitForURL("/");
 
   await page.goto("/timeline");
-  await page.locator("#post-field").fill("test-post");
-  await page.locator("#post [type='submit']").click();
+  await page.locator("#content").fill("test-post");
+  await page.getByRole("button", { name: "Submit" }).click();
   await expect(page.getByText("test post")).toBeVisible();
 
   await page.goto("/logout");
