010561e41845fb4102a6591c80bbe537b0dd888b
Mikuroさいな <10331164+MikuroXina@users.noreply.github.com>
2026-04-20 23:31:18 +0900

    chore: Make logger type json (#687)

--- app/lib/logger.ts
+++ app/lib/logger.ts
@@ -1,4 +1,3 @@
 import { Logger } from "tslog";
 
-// TODO: change type to "json" when we get ready to use log collection service
-export const logger = new Logger({ type: "pretty" });
+export const logger = new Logger({ type: "json" });
